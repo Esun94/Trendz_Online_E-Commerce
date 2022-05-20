c18cf99d205e7bcab487c8c837144887d00c1677
Eric Sun <ericmsun@gmail.com>
2022-05-19 20:59:57 -0400

    created and imported sign-up-form.styles.scss file, imported to sign up form component, created button component

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,15 @@
+// default
+
+// invert
+
+// google sign in
+
+
+
+const Button = ({ children }) => {
+    return (
+        <button className="button-container">{ children }</button>
+    )
+}
+
+export default Button;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
 
+import './sign-up-form.styles.scss'
+
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase utils/firebase.utils';
 
 const defaultFormFields = {
